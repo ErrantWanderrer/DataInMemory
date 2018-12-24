1a30033c89e281b4768111310c7311ac97b34fb8
MorgotBauglir <45843314+MorgotBauglir@users.noreply.github.com>
2018-12-24 21:24:18 +0300

    Create peremen.c

---
+++ peremen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int global1 = 0;
+int global2 = 0;
+
+int main(int argc, char **argv){
+   int *dynamic1 = malloc(sizeof(int));
+   int *dynamic2 = malloc(sizeof(int));
+   
+   int local1 = 0;
+   int local2 = 0;
+   
+   printf("Global variable: %p %p", &global1, &global2);
+   printf("Dynamic variable: %p %p", &dynamic1, &dynamic2
