ced7a6788d0022387e1fd1fd5d405d4bf4161b9c
MorgotBauglir <45843314+MorgotBauglir@users.noreply.github.com>
2018-12-24 22:32:59 +0300

    Update peremen.c

--- peremen.c
+++ peremen.c
@@ -11,5 +11,12 @@ int main(int argc, char **argv){
    int local1 = 0;
    int local2 = 0;
    
-   printf("Global variable: %p %p", &global1, &global2);
-   printf("Dynamic variable: %p %p", &dynamic1, &dynamic2
+   printf("Global variable: %p %p \n", &global1, &global2);
+   printf("Dynamic variable: %p %p \n", dynamic1, dynamic2);
+   printf("Local variable: %p %p \n", &local1, &local2);
+   
+   free(dynamic1);
+   free(dynamic2);
+   
+   return 0;
+}
